b61e7c147d1f54560313017feda51734e346d82f
181355120@qq.com <181355120@qq.com>
2019-02-27 14:50:17 +0800

    添加默认hbase es配置

---
+++ janusgraph-codepipelines-ci/src/test/java/org/janusgraph/codepipelines/TestContainerOne.java
@@ -0,0 +1,29 @@
+package org.janusgraph.codepipelines;
+
+import org.junit.Test;
+
+/**
+ * @author Alexander Patrikalakis
+ */
+public class TestContainerOne {
+    public static class TraversalAlpha {
+        @Test
+        public void testOne() {
+            //no-op
+        }
+        @Test
+        public void testTwo() {
+            //no-op
+        }
+    }
+    public static class TraversalBeta {
+        @Test
+        public void testOne() {
+            //no-op
+        }
+        @Test
+        public void testTwo() {
+            //no-op
+        }
+    }
+}
---
+++ janusgraph-codepipelines-ci/src/test/java/org/janusgraph/codepipelines/TestContainerTwo.java
@@ -0,0 +1,29 @@
+package org.janusgraph.codepipelines;
+
+import org.junit.Test;
+
+/**
+ * @author Alexander Patrikalakis
+ */
+public class TestContainerTwo {
+    public static class TraversalAlpha {
+        @Test
+        public void testOne() {
+            //no-op
+        }
+        @Test
+        public void testTwo() {
+            //no-op
+        }
+    }
+    public static class TraversalBeta {
+        @Test
+        public void testOne() {
+            //no-op
+        }
+        @Test
+        public void testTwo() {
+            //no-op
+        }
+    }
+}
